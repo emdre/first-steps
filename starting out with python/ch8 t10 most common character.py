ba6efe14e9856a01f6b498969f2a1599e2c92700
emdree <magdalena.drej@gmail.com>
2019-07-09 19:39:40 +0200

    code corrected

--- starting out with python/ch8 t10 most common character.py
+++ starting out with python/ch8 t10 most common character.py
@@ -1,5 +1,10 @@
 import collections
 
-text = input('Enter a message: ').lower()
-most_common = (collections.Counter(text).most_common(1))
-print(f'The most common character is {most_common}')
+def main():
+    text = input('Enter a message: ').lower()
+    most_common = (collections.Counter(text).most_common(1))
+    for item in most_common:
+        character = item[0]
+    print(f'The most common character is {character}.')
+
+main()
